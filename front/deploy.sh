fcb0363af4bee041d86b2eb63a79cf312e8c7b55
PabloDinella <pablo.dinella@gmail.com>
2022-02-02 21:48:59 -0300

    update deploy script

--- front/deploy.sh
+++ front/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git checkout gh-pages
-find . ! -name 'dist' -type f -exec rm -f {} +
+find . ! -name 'dist' -type d -exec rm -rf {} +
 mv dist/* ./
 rm -rf dist
 git push origin gh-pages -f
