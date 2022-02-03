a462e2cd4259e28fcf832e4c8f510aead235b87b
PabloDinella <pablo.dinella@gmail.com>
2022-02-02 22:15:16 -0300

    update deploy script

--- front/deploy.sh
+++ front/deploy.sh
@@ -4,6 +4,9 @@ cd ..
 git checkout gh-pages
 rm -rf docs
 cp -r front/dist docs
-# git add .
-# git commit -m "deploy"
-# git push origin gh-pages -f
+git add .
+git commit -m "deploy"
+git push origin gh-pages -f
+git checkout main
+rm -rf docs
+cd front
